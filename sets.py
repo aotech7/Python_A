832419c7d02a7b45e67445c95d0740b49356e05a
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-20 00:52:03 +0530

    sets properties

---
+++ sets.py
@@ -0,0 +1,64 @@
+# myset = {1, 2, 3}
+
+# mydict = {
+#     1: 3,
+#     4: 5,
+#     9: 10,
+# }
+
+# newset = {}
+# print(type(newset))
+
+# union 
+
+# both are for union 
+# s = s1 | s2 
+# print(s1.union(s2))
+
+# both are for intersection 
+# s = s1 & s2 
+# print(s)
+# print(s1.intersection(s2))
+
+# both are differences 
+# s = s1 - s2 
+# print(s)
+# print(s1.difference(s2))
+
+# s = (s1 | s2) - (s1 & s2)
+# print(s)
+# print(s1.symmetric_difference(s2))
+# s = s1.union(s2) - s1.intersection(s2)
+# print(s)
+
+# cardinality 
+
+# print(len(s1))
+
+# s = {}
+
+# news = set(s)
+
+# s = set()
+# print(type(s))
+
+# Disjoint sets 
+
+# s1 = {1, 2, 3}
+# s2 = {2, 3, 4}
+
+# intersect = s1 & s2 
+# if len(intersect) > 0: 
+#     print("It is not disjoint")
+# else:
+#     print("It is disjoint")
+
+# Q. find all the unique elements from this list 
+# mylist = [1, 2, 2, 2, 3, 4]
+# s = set(mylist)
+# print(s)
+
+# set() will convert every iterable into set 
+# mystring = "aaaaabshaaaaaabbbbsssiohi"
+# s = set(mystring)
+# print(s)
